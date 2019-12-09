cac12988cfd6c278a531ed45fd95c05a4a4f65df
alex-moon <alex.moon@kakaocorp.com>
2019-12-09 11:50:03 +0900

    로대시 추가 스태틱 폴더 분리

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.use(express.static('app'));
-server.use('/',express.static(__dirname + '/app/index.html'));
+server.use('/',express.static(__dirname + '/static/index.html'));
 
 server.listen(3000, function () {
 console.log('ALEACT DEV SERVER listening on port 3000!');
